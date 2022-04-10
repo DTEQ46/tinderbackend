b292d295c52d47a613433684ef745866af1a3aab
DTEQ <olasehindedaniel109@gmail.com>
2022-04-09 23:02:18 -0400

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-import express from "express";
-import mongoose from "mongoose";
-import Cards from "./dbCards.js";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import Cards from './dbCards.js';
+import cors from 'cors';
 
 //App Config
 const app = express();
 const port = process.env.PORT || 8001;
-  
 
 //Middlewares
 
@@ -14,27 +13,27 @@ app.use(express.json());
 app.use(cors());
 
 // DB config
-const username = "admin";
-const password = "dteq46";
-const cluster = "cluster0.uiaxj";
-const dbname = "tinderdb";
+const username = 'admin';
+const password = 'dteq46';
+const cluster = 'cluster0.1pfcg';
+const dbname = 'tinderdb';
 mongoose.connect(
-  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`
 );
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('Connected successfully');
 });
 
 //API Endpoints
 
-app.get("/", (req, res) =>
-  res.status(200).send("Welcome to Backend Programming!!!")
+app.get('/', (req, res) =>
+  res.status(200).send('Welcome to Backend Programming!!!')
 );
 
-app.post("/tinder/cards", async (request, response) => {
+app.post('/tinder/cards', async (request, response) => {
   const user = new Cards(request.body);
 
   try {
@@ -45,7 +44,7 @@ app.post("/tinder/cards", async (request, response) => {
   }
 });
 
-app.get("/tinder/cards", async (request, response) => {
+app.get('/tinder/cards', async (request, response) => {
   const users = await Cards.find({});
 
   try {
